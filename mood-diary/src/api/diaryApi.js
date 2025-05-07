0c9310cba31d3c3fcbbb02db90f4e98b50945705
LuciferDevV <164545857+LuciferDevV@users.noreply.github.com>
2025-05-07 18:19:58 +0300

    Delete mood-diary/src directory

--- mood-diary/src/api/diaryApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://6818d7905a4b07b9d1d10d74.mockapi.io/entries';
-
-export const getEntries = () => axios.get(BASE_URL);
-export const getEntryById = (id) => axios.get(`${BASE_URL}/${id}`);
-export const createEntry = (data) => axios.post(BASE_URL, data);
-export const updateEntry = (id, data) => axios.put(`${BASE_URL}/${id}`, data);
-export const deleteEntry = (id) => axios.delete(`${BASE_URL}/${id}`);
